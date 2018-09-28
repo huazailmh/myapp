c8ab3fda78789a85a266972e4233c9267fd9df7a
i321649 <bruce.li03@sap.com>
2018-09-28 14:55:00 +0800

    display excel when on loading

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 const express = require('express')
 const app = express()
 
@@ -8,5 +10,11 @@ app.use("/js", express.static('js'))
 app.use("/styles", express.static('styles'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get("/reports/r001", function(req, res) {
+    var contents = fs.readFileSync('reporting_system2.xlsx');
+    //console.log(contents);
+    res.send(Buffer.from(contents).toString("base64"));
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
