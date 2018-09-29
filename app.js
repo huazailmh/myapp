1209da5eaf2632573c2fcc23a83fb672c0ab84de
i321649 <bruce.li03@sap.com>
2018-09-29 14:47:22 +0800

    evaluate report display

--- app.js
+++ app.js
@@ -14,7 +14,11 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.get("/reports/r001", function(req, res) {
     var contents = fs.readFileSync('reporting_system2.xlsx');
     //console.log(contents);
-    res.send(Buffer.from(contents).toString("base64"));
+    var reportData = {};
+    reportData.reportId = "r001";
+    reportData.reportDataSet = {"dsContent": { "expense": 200, "income": 710, "profit": 510, "comments": "haha good"}, "dsAdjustContent": {}};
+    reportData.reportTemplate = Buffer.from(contents).toString("base64");
+    res.send(reportData);
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
